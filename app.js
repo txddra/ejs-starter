c104d1f5683d678234adb80e3514406620945398
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-10-19 11:01:17 -0400

    changed port to 30001

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use('/ejs', require('./routes/ejsRoutes'));
-const port = 3000;
+const port = 3001;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
